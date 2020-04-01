e17c11b1a6d0f41fe6878ce1443e1c06168ea90b
James Roman <platinio94@gmail.com>
2020-03-31 21:09:30 -0300

    create base actor script

---
+++ Assets/Main/Scripts/Character/Actor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Gamaga
+{
+    public class Actor : MonoBehaviour
+    {
+        private Rigidbody2D rb = null;
+        private SpriteRenderer render = null;
+        private Animator animator = null;
+
+        private void Awake()
+        {
+            GetReferences();
+        }
+
+        private void GetReferences()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            render = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+        }        
+
+    }
+}
+
