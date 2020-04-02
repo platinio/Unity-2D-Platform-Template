cf024036b5484347b06a5fad4ecfd1e8c0b6b7f4
James Roman <platinio94@gmail.com>
2020-04-01 23:55:36 -0300

    refactoring camera script

--- Assets/Main/Scripts/CameraController.cs
+++ Assets/Main/Scripts/CameraController.cs
@@ -6,26 +6,25 @@ namespace Gamaga
     public class CameraController : MonoBehaviour
     {
         [SerializeField] private Transform target = null;
-        [SerializeField] [Range(1.0f, 10.0f)] private float zoom = 5.0f;
         [SerializeField] [Range(0.1f, 2.0f)] private float followSmooth = 2.0f;
         [SerializeField] private Vector3 offset = Vector2.zero;
         [SerializeField] private float mirrorTime = 0.25f;
 
         private Vector2 velocity = Vector2.zero;
-        private Camera thisCamera = null;
         private Vector2 lastTargetPostion = Vector2.zero;
-        private Vector2 currentTargetDirection = Vector2.zero;
         private float mirrorTimer = 0.0f;
         private int cameraFacingDirection = 1;
 
-        private void Awake()
-        {
-            thisCamera = GetComponent<Camera>();
-        }
 
         private void LateUpdate()
         {
-            if (GetTargetMovingDirection() != cameraFacingDirection)
+            MirrorUpdate();
+            FollowTarget();            
+        }
+
+        private void MirrorUpdate()
+        {
+            if ( ShouldMirrorCamera() )
             {
                 mirrorTimer += Time.deltaTime;
 
@@ -39,10 +38,23 @@ namespace Gamaga
             {
                 mirrorTimer = 0.0f;
             }
+        }
 
-            Vector3 desirePosition = Vector2.SmoothDamp(transform.position, CalculateCameraDesirePosition(), ref velocity, followSmooth);
-            desirePosition.z = transform.position.z;
-            transform.position = desirePosition;
+        private bool ShouldMirrorCamera()
+        {
+            return GetTargetMovingDirection() != cameraFacingDirection;
+        }
+
+        private void FollowTarget()
+        {
+            Vector3 smoothPosition = CalculateCameraSmoothPosition();
+            smoothPosition.z = transform.position.z;
+            transform.position = smoothPosition;
+        }
+
+        private Vector2 CalculateCameraSmoothPosition()
+        {
+            return Vector2.SmoothDamp(transform.position, CalculateCameraDesirePosition(), ref velocity, followSmooth);
         }
 
         private Vector2 CalculateCameraDesirePosition()
