5aef9c6a47447ee2d9f255f9659e78ee3b0820c5
James Roman <platinio94@gmail.com>
2020-04-04 11:18:42 -0300

    working on deadgame logic

---
+++ Assets/Main/Scripts/GameLogic/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Gamaga
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer playerSR = null;
+        [SerializeField] private SpriteRenderer gameOverSR = null;
+
+        private const string GameOverLayerName = "GameOver";
+
+        public void GameOver()
+        {
+            gameOverSR.Fade(0.9f , 2.0f).SetEase(Ease.EaseOutExpo).SetEvent(delegate 
+            {                
+                playerSR.sortingLayerName = GameOverLayerName;
+                playerSR.sortingLayerID = 1;
+            } , 0.5f ).SetDelay(2.0f).SetOnComplete(delegate { Time.timeScale = 0.0f; });
+        }
+    }
+
+}
+
