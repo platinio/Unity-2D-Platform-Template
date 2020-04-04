6cd89e91a22995a4eb93a49df1b5f25134d28c80
James Roman <platinio94@gmail.com>
2020-04-04 15:06:49 -0300

    virtual joystick for mobile support

---
+++ Assets/Main/Scripts/Input/Joystick.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+namespace Gamaga.InputSystem
+{
+	public enum MovementDirection
+	{
+		Vertical,
+		Horizontal,
+		Both
+	}
+
+	/// <summary>
+	/// Simple virtual joystick
+	/// </summary>
+	public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+	{
+
+		#region INSPECTOR
+		[SerializeField] MovementDirection m_movementDirection = MovementDirection.Both;
+		[SerializeField] private float m_movementRange = 40.0f;
+		[SerializeField] private string m_horizontalAxisName = "Horizontal";
+		[SerializeField] private string m_verticalAxisName = "Vertical";
+		[SerializeField] private Image m_base = null;
+		[SerializeField] private Image m_stick = null;
+		[SerializeField] private bool m_hideOnRelease = false;
+		[SerializeField] private bool m_useDistance = false;
+		#endregion
+
+		#region PRIVATE
+		private RectTransform m_stickRect = null;
+		private RectTransform m_baseRect = null;
+		private Vector2 m_stickCenterPos = Vector2.zero;
+		private Vector2 m_stickInitialPos = Vector2.zero;
+		private Vector2 m_baseInitialPos = Vector2.zero;
+		#endregion
+
+		void Start()
+		{
+			//get references
+			m_stickRect = m_stick.GetComponent<RectTransform>();
+			m_baseRect = m_base.GetComponent<RectTransform>();
+
+			//initialize values
+			m_stickCenterPos = m_stickRect.position;
+			m_stickInitialPos = m_stickRect.position;
+			m_baseInitialPos = m_baseRect.position;
+
+			//hide the joystick
+			if (m_hideOnRelease)
+				Hide(true);
+
+			//create the axis
+			JoystickAxis.CreateAxis(m_horizontalAxisName);
+			JoystickAxis.CreateAxis(m_verticalAxisName);
+		}
+
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			//set joystick pos
+			Vector2 stickPos = eventData.position;
+
+
+			//restric joystick movement
+			if (m_movementDirection == MovementDirection.Vertical)
+			{
+				stickPos.x = m_stickCenterPos.x;
+			}
+			else if (m_movementDirection == MovementDirection.Horizontal)
+			{
+				stickPos.y = m_stickCenterPos.y;
+			}
+
+
+
+			Vector2 difference = new Vector2(stickPos.x, stickPos.y) - m_stickCenterPos;
+			float diffMagnitude = difference.magnitude;
+			Vector2 normalizedDifference = difference / diffMagnitude;
+
+			// if the joystick is out of range
+			if (diffMagnitude > m_movementRange)
+			{
+				stickPos = m_stickCenterPos + normalizedDifference * m_movementRange;
+			}
+
+			Vector2 m = new Vector2(m_stickRect.position.x, m_stickRect.position.y) - new Vector2(m_baseRect.position.x, m_baseRect.position.y);
+			m.Normalize();
+
+			//if we use distance, this is most commonf for vehicles or running etc
+			if (m_useDistance)
+			{
+				//distance from center to stcik
+				float distance = Vector2.Distance(stickPos, m_baseRect.position);
+				m = new Vector2(m.x * (distance / m_movementRange), m.y * (distance / m_movementRange));
+			}
+
+
+
+			//updateaxis value
+			JoystickAxis.SetAxis(m_horizontalAxisName, m.x);
+			JoystickAxis.SetAxis(m_verticalAxisName, m.y);
+			m_stickRect.position = stickPos;
+		}
+
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			//set valus to 0
+			JoystickAxis.SetAxis(m_horizontalAxisName, 0.0f);
+			JoystickAxis.SetAxis(m_verticalAxisName, 0.0f);
+
+			m_stickRect.anchoredPosition = Vector2.zero;
+			m_baseRect.position = m_baseInitialPos;
+
+			if (m_hideOnRelease)
+				Hide(true);
+		}
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			//hide joystick
+			if (m_hideOnRelease)
+			{
+				Hide(false);
+			}
+
+			m_baseRect.position = eventData.position; ;
+			m_stickRect.position = eventData.position;
+			m_stickCenterPos = eventData.position;
+
+		}
+
+
+		private void Hide(bool hide)
+		{
+			m_stick.gameObject.SetActive(!hide);
+			m_base.gameObject.SetActive(!hide);
+		}
+
+
+	}
+
+}
+
---
+++ Assets/Main/Scripts/Input/JoystickAxis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamaga.InputSystem
+{
+
+	public static class JoystickAxis
+	{
+		private static Dictionary<string, float> m_axis = new Dictionary<string, float>();
+
+		public static void CreateAxis(string axisName)
+		{
+			m_axis[axisName] = 0.0f;
+		}
+
+		public static float GetAxis(string axisName)
+		{
+			return m_axis[axisName];
+		}
+
+		public static void SetAxis(string axisName, float value)
+		{
+			m_axis[axisName] = value;
+		}
+
+	}
+
+}
